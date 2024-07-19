9695e5d94ef438a6ddefa73130ca7ff1114aea71
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2024-07-19 04:24:28 +0000

    refactor: use `remap` of `extra-utils`

--- src/map-to-range.ts
+++ src/map-to-range.ts
@@ -1,9 +1,12 @@
+import { remap } from 'extra-utils'
+
+/**
+ * @deprecated use `remap` of `extra-utils` instead
+ */
 export function mapToRange(
   value: number
 , oldMin: number, oldMax: number
 , newMin: number, newMax: number
 ): number {
-  return (value - oldMin) / (oldMax - oldMin)
-       * (newMax - newMin)
-       + newMin
+  return remap(value, [oldMin, oldMax], [newMin, newMax])
 }
